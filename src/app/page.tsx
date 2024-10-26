bc18f76a09b470ef66162247ba8941e5fc093698
Lokeshranjan <lokeshranjan.86@gmail.com>
2024-10-26 22:29:41 +0530

    evryhting working fine just need to setp up pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,15 @@ import HeroSection from "@/components/heroSection";
 import HowItWorks from "@/components/HowItWorks";
 import Header from "@/components/navbar";
 import Payment from "@/components/payment";
-
 export default function Home() {
   return (
 
     <div>
       <Header />
-      <HeroSection />
+      {/* <HeroSection />
       <HowItWorks />
-      <Footer />
+      <Footer /> */}
       <Payment />
-
-
     </div>
     );
 }
