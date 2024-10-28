3f1b0c45b37cf3372d14bdc47ae2b90a6a54ba27
asmb123 <asmit.barua.phe23@itbhu.ac.in>
2024-10-28 17:41:05 +0530

    code cleaning

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 import Footer from "@/components/Footer";
 import HeroSection from "@/components/heroSection";
 import HowItWorks from "@/components/HowItWorks";
-import Header from "@/components/navbar";
 import Payment from "@/components/payment";
 export default function Home() {
   return (
 
     <div>
-      <Header />
       <HeroSection />
       <HowItWorks />
       <Footer />
